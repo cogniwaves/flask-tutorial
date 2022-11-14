ca97c3161c55bc2d79fbbe2ce153765dfafb63f9
Eric (poiqwepoi) <eric@cogniwaves.com>
2022-11-13 19:49:07 -0500

    Adding Blog Blueprints

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,3 +35,12 @@ def create_app(test_config=None):
 
     return app
 
+def create_app():
+    app = ...
+    # existing code omitted
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
