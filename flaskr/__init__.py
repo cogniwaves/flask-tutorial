deb583df636b3fca2e5b1cd8995a39ef140e0933
Eric (poiqwepoi) <eric@cogniwaves.com>
2022-11-17 23:26:36 -0500

    Finished Project Tested

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -33,14 +33,14 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    return app
-
-def create_app():
-    app = ...
-    # existing code omitted
 
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
     return app
+
+    return app
+
+
+
